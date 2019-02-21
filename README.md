bab2be381d830c2eb9cfbc2509bd9524f3b43382
mega virtualbox <mega@example.com>
2019-02-20 20:49:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## How to run the bascripts example: 
+
+* sh sytem_info.sh
+
+OR
+
+* . ./rvm_source.sh
---
+++ system_info.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+
+echo “This information is brought to you by $0.”
+
+#Welcome the user
+echo “Welcome, $USER”
+echo
+echo “Today is `date`.”
+echo
+
+#Currently active users.
+echo “Following users are presently active:”
+w | cut -d ' ' -f 1 | grep -v USER | sort -u
+echo
+
+#System information using uname command
+echo “This is `uname -s` running on a `uname -m` processor.”
+echo
+
+#Information of uptime
+echo “Following is the uptime information:”
+uptime
+echo
+
+#Showing free memory
+echo “Memory Details:”
+free
+echo
+
+#Disk space usage using df command
+echo “Disk Space Utilization:”
+df -mh
+echo 
+
